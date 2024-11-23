85d612fadd8908abe34d954c840d52e08edc2bee
x42bn6 <x42bn6@gmail.com>
2024-11-23 01:44:12 +0000

    Pair group stage done (?)

--- ept.py
+++ ept.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from math import floor
 
 from ortools.sat.python.cp_model import IntVar
 
 from metadata import Metadata
-from stage import GroupStage, Stage, Tournament
+from stage import GroupStage, Stage, Tournament, PairGroupStage
 
 
 class EptStage:
@@ -48,6 +49,31 @@ class EptGroupStage(EptStage, ABC):
     def get_points(self) -> [IntVar]:
         pass
 
+class EptPairGroupStage(EptStage, ABC):
+    def __init__(self,
+                 stage: PairGroupStage,
+                 points: [int]):
+        super().__init__(stage, points)
+        self.obtained_points: [IntVar] = []
+        self.build()
+
+    def build(self):
+        metadata: Metadata = self.stage.metadata
+        team_database = metadata.team_database
+        self.obtained_points: [IntVar] = [metadata.model.new_int_var(0, 99999, f'd_{self.stage.name}_{i}')
+                                          for i in range(len(team_database.get_all_teams()))]
+
+        points_extended = [self.points[floor(p / 2)] if floor(p / 2) < len(self.points) else 0 for p in
+                               range(self.stage.team_count)]
+
+        for team in team_database.get_all_teams():
+            team_index = team_database.get_team_index(team)
+            self.obtained_points[team_index] = sum(
+                self.stage.indicators[team_index][p] * points_extended[p] for p in range(len(points_extended)))
+        pass
+
+    def get_points(self) -> [IntVar]:
+        pass
 
 class EptTournament:
     def __init__(self,
--- testsuite.py
+++ testsuite.py
@@ -2,27 +2,106 @@ from ortools.constraint_solver.pywrapcp import BooleanVar
 from ortools.sat.python import cp_model
 from ortools.sat.python.cp_model import CpModel, IntVar
 
-from ept import EptGroupStage, EptTournament
+from ept import EptGroupStage, EptTournament, EptPairGroupStage
 from metadata import Metadata
-from stage import Root, GroupStage, Tournament
+from stage import Root, GroupStage, Tournament, PairGroupStage
 from teams import Team, TeamDatabase
 
 
 def main():
+    # basic_two_group_stage()
+    pair_group_stage_into_single_group_stage()
+
+
+def pair_group_stage_into_single_group_stage():
+    teams: [Team] = [
+        Team("A"),
+        Team("B"),
+        Team("C"),
+        Team("D"),
+        Team("E"),
+        Team("F"),
+        Team("G"),
+        Team("H")
+    ]
+    team_database: TeamDatabase = TeamDatabase()
+    for team in teams:
+        team_database.add_team(team)
+
+    max_objective_value = -1
+    for team in team_database.get_all_teams():
+        model: CpModel = CpModel()
+        metadata = Metadata(team_database, model)
+
+        root: Root = Root("root", len(teams), metadata, teams=teams)
+        group_stage_1: PairGroupStage = PairGroupStage("group_stage_1",
+                                                       4,
+                                                       2,
+                                                       metadata,
+                                                       group_a=team_database.get_teams_by_names("A", "B", "C", "D"),
+                                                       group_b=team_database.get_teams_by_names("E", "F", "G", "H")
+                                                       )
+        group_stage_2: GroupStage = GroupStage("group_stage_2", 4, 0, metadata)
+        tournament: Tournament = Tournament("tournament", group_stage_1, metadata)
+
+        root.bind_forward(group_stage_1)
+        group_stage_1.bind_forward(group_stage_2)
+
+        root.build()
+        group_stage_1.build()
+        group_stage_2.build()
+        tournament.build()
+
+        ept_group_stage_1: EptPairGroupStage = EptPairGroupStage(group_stage_1, [300, 100, 50])
+        ept_group_stage_1.build()
+        ept_group_stage_2: EptGroupStage = EptGroupStage(group_stage_2, [300])
+        ept_group_stage_2.build()
+        ept_tournament: EptTournament = EptTournament(tournament, [1000, 500, 250, 100, 50, 25, 10, 5])
+        ept_tournament.build()
+
+        print(f"Now optimising for {team.name}")
+
+        # Optimise
+        total_points = [
+            ept_group_stage_1.obtained_points[t_index] +
+            ept_group_stage_2.obtained_points[t_index] +
+            ept_tournament.obtained_points[t_index]
+            for t in teams
+            if (t_index := team_database.get_team_index(t)) is not None
+        ]
+
+        add_optimisation_constraints(model, team, team_database, teams, total_points)
+
+        solver = cp_model.CpSolver()
+        status = solver.Solve(model)
+        if status != cp_model.OPTIMAL:
+            print(f"Team {team.name} probably cannot finish in top 2")
+            continue
+
+        if solver.objective_value > max_objective_value:
+            max_objective_value = solver.objective_value
+
+            for t in teams:
+                t_index = team_database.get_team_index(t)
+                print(f"Team {t.name} points: {solver.value(total_points[t_index])}")
+                print(f"Team {t.name} GS1 points: {solver.value(ept_group_stage_1.obtained_points[t_index])}")
+                print(f"Team {t.name} GS2 points: {solver.value(ept_group_stage_2.obtained_points[t_index])}")
+                print(f"Team {t.name} Tournament points: {solver.value(ept_tournament.obtained_points[t_index])}")
+            print(max_objective_value)
+
+
+def basic_two_group_stage():
     teams: [Team] = [
         Team("A"),
         Team("B"),
         Team("C"),
         Team("D")
     ]
-
     team_database: TeamDatabase = TeamDatabase()
     for team in teams:
         team_database.add_team(team)
 
     max_objective_value = -1
-    max_solver = None
-    max_team = None
     for team in team_database.get_all_teams():
         model: CpModel = CpModel()
         metadata = Metadata(team_database, model)
@@ -31,8 +110,10 @@ def main():
         group_stage_1: GroupStage = GroupStage("group_stage_1", len(teams), 2, metadata)
         group_stage_2: GroupStage = GroupStage("group_stage_2", 2, 0, metadata)
         tournament: Tournament = Tournament("tournament", group_stage_1, metadata)
+
         root.bind_forward(group_stage_1)
         group_stage_1.bind_forward(group_stage_2)
+
         root.build()
         group_stage_1.build()
         group_stage_2.build()
@@ -56,23 +137,7 @@ def main():
             if (t_index := team_database.get_team_index(t)) is not None
         ]
 
-        team_count_range = range(len(teams))
-        ranks: [IntVar] = {team: model.NewIntVar(1, len(teams), f'ranks_{team}') for team in team_count_range}
-        aux: [[BooleanVar]] = {(i, j): model.NewBoolVar(f'aux_{i}_{j}') for i in team_count_range for j in
-                               team_count_range}
-        big_m: int = 20000
-        for i in team_count_range:
-            for j in team_count_range:
-                if i == j:
-                    model.Add(aux[(i, j)] == 1)
-                else:
-                    model.Add(total_points[i] - total_points[j] <= (1 - aux[(i, j)]) * big_m)
-                    model.Add(total_points[j] - total_points[i] <= aux[(i, j)] * big_m)
-            ranks[i] = sum(aux[(i, j)] for j in team_count_range)
-
-        team_index: int = team_database.get_team_index(team)
-        model.Add(ranks[team_index] > 2)
-        model.Maximize(total_points[team_index])
+        add_optimisation_constraints(model, team, team_database, teams, total_points)
 
         solver = cp_model.CpSolver()
         status = solver.Solve(model)
@@ -89,10 +154,26 @@ def main():
                 print(f"Team {t.name} GS1 points: {solver.value(ept_group_stage_1.obtained_points[t_index])}")
                 print(f"Team {t.name} GS2 points: {solver.value(ept_group_stage_2.obtained_points[t_index])}")
                 print(f"Team {t.name} Tournament points: {solver.value(ept_tournament.obtained_points[t_index])}")
-
-            max_solver = solver
-            max_team = team
             print(max_objective_value)
+
+
+def add_optimisation_constraints(model, team, team_database, teams, total_points):
+    team_count_range = range(len(teams))
+    ranks: [IntVar] = {team: model.NewIntVar(1, len(teams), f'ranks_{team}') for team in team_count_range}
+    aux: [[BooleanVar]] = {(i, j): model.NewBoolVar(f'aux_{i}_{j}') for i in team_count_range for j in
+                           team_count_range}
+    big_m: int = 20000
+    for i in team_count_range:
+        for j in team_count_range:
+            if i == j:
+                model.Add(aux[(i, j)] == 1)
+            else:
+                model.Add(total_points[i] - total_points[j] <= (1 - aux[(i, j)]) * big_m)
+                model.Add(total_points[j] - total_points[i] <= aux[(i, j)] * big_m)
+        ranks[i] = sum(aux[(i, j)] for j in team_count_range)
+    team_index: int = team_database.get_team_index(team)
+    model.Add(ranks[team_index] > 2)
+    model.Maximize(total_points[team_index])
 
 
 main()
