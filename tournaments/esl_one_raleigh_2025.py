9810c4ef51f785693d2ef1a0d004bcb00362803b
x42bn6 <x42bn6@gmail.com>
2025-04-11 22:49:31 +0100

    BB > NGX

--- tournaments/esl_one_raleigh_2025.py
+++ tournaments/esl_one_raleigh_2025.py
@@ -55,6 +55,7 @@ class EslOneRaleigh2025:
         esl_one_ral_2025_playoff.ubsf_2.set_winner("Team Spirit")
 
         esl_one_ral_2025_playoff.lbr1_1.set_winner("Team Liquid")
+        esl_one_ral_2025_playoff.lbr1_2.set_winner("BetBoom Team")
 
         esl_one_ral_2025_gs.build()
         esl_one_ral_2025_playoff.build()
