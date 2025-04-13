1289b603b1f9864e1179cba6dae5d1d89e4e0067
x42bn6 <x42bn6@gmail.com>
2025-04-13 16:39:23 +0100

    PARIVISION > BB

--- tournaments/esl_one_raleigh_2025.py
+++ tournaments/esl_one_raleigh_2025.py
@@ -62,6 +62,8 @@ class EslOneRaleigh2025:
         esl_one_ral_2025_playoff.lbqf_1.set_winner("PARIVISION")
         esl_one_ral_2025_playoff.lbqf_2.set_winner("BetBoom Team")
 
+        esl_one_ral_2025_playoff.lbsf.set_winner("PARIVISION")
+
         esl_one_ral_2025_gs.build()
         esl_one_ral_2025_playoff.build()
         esl_one_ral_2025.build()
