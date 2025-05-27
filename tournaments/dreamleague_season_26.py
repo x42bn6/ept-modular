4e193b26dd2d90e7c0b88f89a275a86190c9f2a4
x42bn6 <x42bn6@gmail.com>
2025-05-27 20:57:34 +0100

    Retroactive - Aurora Gaming cannot finish top 1 in GS2 in DL S26

--- tournaments/dreamleague_season_26.py
+++ tournaments/dreamleague_season_26.py
@@ -63,10 +63,10 @@ class DreamLeagueSeason26:
 
         dl_s26_gs2.team_can_finish_between("PARIVISION", 1, 4)
         dl_s26_gs2.team_can_finish_between("BetBoom Team", 1, 8)
-        dl_s26_gs2.team_can_finish_between("Aurora Gaming", 1, 8)
-        dl_s26_gs2.team_can_finish_between("Gaimin Gladiators", 1, 8)
         dl_s26_gs2.team_can_finish_between("Talon Esports", 1, 8)
-        dl_s26_gs2.team_can_finish_between("Yakult Brothers", 1, 8)
+        dl_s26_gs2.team_can_finish_between("Aurora Gaming", 2, 8)
+        dl_s26_gs2.team_can_finish_between("Gaimin Gladiators", 1, 8)
+        dl_s26_gs2.team_can_finish_between("Yakult Brothers", 2, 8)
         dl_s26_gs2.team_can_finish_between("Nigma Galaxy", 3, 8)
         dl_s26_gs2.team_can_finish_between("Team Liquid", 3, 8)
 
